f7a79f423be32acda7ca227638c33176bf0df5c6
pavlo <iris398322@gmail.com>
2025-01-08 18:07:44 +0200

    connects S3 bucket, implements upload and download operation

--- file-service/src/app.controller.ts
+++ file-service/src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Param, UploadedFile, UseInterceptors} from '@nestjs/common';
 import { AppService } from './app.service';
-
+import {FileInterceptor} from "@nestjs/platform-express";
+import {MessagePattern, Payload} from "@nestjs/microservices";
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  constructor(private readonly appService: AppService) {
+
   }
+
+  @MessagePattern({cmd : "upload_file"})
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@Payload() data: { file: Express.Multer.File }) {
+    return this.appService.uploadFile(data.file.buffer, data.file.originalname);
+  }
+
+
+  @MessagePattern({cmd : "get_profile_images"})
+  async getProfileImage(@Payload() data : {image_ids : string[]}){
+
+  }
+
+  @MessagePattern({cmd : "get_post_images"})
+  async getPostImages(@Payload() data : {image_ids: string[]}){
+
+  }
+
+  @MessagePattern({cmd : "get_signed_url"})
+  async getSignedUrl(@Payload() data: { key: string }) {
+    return this.appService.getSignedUrl(data.key);
+  }
+
+
 }
--- file-service/src/app.module.ts
+++ file-service/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ConfigModule} from '@nestjs/config';
+import { S3Service } from './s3/s3.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, S3Service],
 })
 export class AppModule {}
