3f0f76813f8e6b0014657fd5f7897c9f3dc07c9e
pavlo <iris398322@gmail.com>
2025-01-11 18:39:15 +0200

    refactoring and api specification in asyncapi.yaml

--- token-service/src/app.controller.ts
+++ token-service/src/app.controller.ts
@@ -1,42 +1,42 @@
-import {BadRequestException, Controller, Get, HttpException, InternalServerErrorException} from '@nestjs/common';
-import { AppService } from './app.service';
-import {MessagePattern, RpcException} from "@nestjs/microservices";
+import {BadRequestException, Controller, HttpException, InternalServerErrorException} from '@nestjs/common';
+import {AppService} from './app.service';
+import {EventPattern, MessagePattern, RpcException} from "@nestjs/microservices";
 import {ITokensResponse} from "src/interfaces/ITokensResponse";
+import {IAuthPayload} from "src/interfaces/IAuthPayload";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @MessagePattern({cmd: 'generate_token'})
-  async generateToken(user: UserDTO) : Promise<{tokens : ITokensResponse}> {
+  @MessagePattern({cmd: 'generate_tokens'})
+  async generateTokens(payload: UserDTO) : Promise<{tokens : ITokensResponse}> {
 
-    if (!user.id) {
+    if (!payload.id) {
       throw new BadRequestException('User id is required');
     }
 
-    const tokens = await this.appService.generateTokens(user);
+    const tokens = await this.appService.generateTokens(payload);
     console.log(tokens);
     return {tokens};
   }
 
 
   @MessagePattern({cmd: 'verify_token'})
-  async validateToken(token: string): Promise<{ valid: boolean; payload?: any }> {
+  async validateToken(payload : {access_token: string}): Promise<{ valid: boolean; payload?: IAuthPayload }> {
     try {
-      const payload = await this.appService.verifyAccessToken(token);
-      return {valid: true, payload};
+      const response = await this.appService.verifyAccessToken(payload.access_token);
+      return {valid: true, payload : response};
     } catch (e) {
       console.error(e)
-      return {valid: false};
+      return {valid: false, payload : null};
     }
   }
 
-  @MessagePattern({cmd: 'refresh_tokens'})
-  async refreshToken(token: string): Promise<{tokens : ITokensResponse}> {
+  @MessagePattern('refresh_tokens')
+  async refreshToken(payload : {refresh_token: string}): Promise<ITokensResponse> {
     try {
-      const payload = await this.appService.verifyRefreshToken(token);
-      const tokens = await this.appService.generateTokens(payload)
-      return {tokens}
+      const response = await this.appService.verifyRefreshToken(payload.refresh_token);
+      return await this.appService.generateTokens(response)
     } catch (e) {
 
       if(e !instanceof HttpException) {
@@ -46,12 +46,12 @@ export class AppController {
     }
   }
 
-  @MessagePattern({cmd: 'blacklist_token'})
-  async blacklistToken(tokens : {refreshToken : string, accessToken : string}): Promise<{ status : string }> {
+  @EventPattern('blacklist_tokens')
+  async blacklistToken(payload : {refresh_token : string, access_token : string}): Promise<void> {
+    //TODO: use event pattern here. Handle errors properly.
     console.log('in blacklistToken controller');
     try {
-      await this.appService.blackListTokens(tokens.accessToken, tokens.refreshToken);
-      return {status : 'ok'}
+      await this.appService.blackListTokens(payload.access_token, payload.refresh_token);
     } catch (e) {
       console.error(e)
       throw new RpcException(e)
